8a228e01084c23bc425795aa0fd256ff6c924891
abhinav333 <kulkarni.Abhinav91@gmail.com>
2024-08-17 19:42:42 -0400

    Update matrix_multiply_fixed.h

--- HLS/matrix_multiply_fixed.h
+++ HLS/matrix_multiply_fixed.h
@@ -80,21 +80,7 @@ typedef hls::x_complex<CMPLX_TYPE > MATRIX_T;
 typedef hls::x_traits<MATRIX_T,MATRIX_T>::MADD_T TEST;
 typedef hls::x_complex<TEST > MATRIX_T2;
 typedef hls::stream<MATRIX_T > MATRIX_T_STREAM;
-//typedef hls::x_complex<MULT_RT, CMPLX_TYPE > MATRIX_T_D;
-//typedef hls::x_complex<float> MATRIX_T;
-/*
-// Define configuration class for each implementation "target"
-struct MULT_CONFIG_SMALL: hls::matrix_multiply_traits<hls::NoTranspose,hls::NoTranspose,A_ROWS,A_COLS,B_ROWS,B_COLS,MATRIX_T, MATRIX_T>{
-  static const int INNER_II = 4;
-};
-struct MULT_CONFIG_FAST: hls::matrix_multiply_traits<hls::NoTranspose,hls::NoTranspose,A_ROWS,A_COLS,B_ROWS,B_COLS,MATRIX_T, MATRIX_T>{
-  static const int ARCH = 2;
-  static const int UNROLL_FACTOR = 4;
-};
-struct MULT_CONFIG_FASTER: hls::matrix_multiply_traits<hls::NoTranspose,hls::NoTranspose,A_ROWS,A_COLS,B_ROWS,B_COLS,MATRIX_T, MATRIX_T>{
-  static const int ARCH = 4;
-};
-*/
+
 struct MULT_CONFIG_FAST: hls::matrix_multiply_traits<hls::NoTranspose,hls::NoTranspose,A_ROWS,A_COLS,B_ROWS,B_COLS,MATRIX_T, MATRIX_T>{
   static const int ARCH = 2;
   static const int UNROLL_FACTOR = 4;
@@ -152,17 +138,6 @@ void adm_frame(MATRIX_T cluster_mat1[CLUSTERS][K][ANTENNA_PER_CLUSTER],
 		 MATRIX_T cluster_mat3[CLUSTERS][K][K],
 		 MATRIX_T y[CLUSTERS][ANTENNA_PER_CLUSTER][1],
 		 MATRIX_T x_est[CLUSTERS][K][1]);
-void dcd_frame(MATRIX_T y_c[FRAMES][CLUSTERS][ANTENNA_PER_CLUSTER][1],
-MATRIX_T x_est[FRAMES][K][1]);
-void dcd_cluster(MATRIX_T muc[K][1],
-			MATRIX_T nuc[K][1],
-			MATRIX_T cluster_mat1_cd[ANTENNA_PER_CLUSTER][K],
-			MATRIX_T y_c[ANTENNA_PER_CLUSTER][1],
-			MATRIX_T x_cluster_estimate[K][1],int cl);
-void dcd_acc_core(MATRIX_T x_cluster_estimate[CLUSTERS][K][1],MATRIX_T x[K][1]);
-//			void dcd_algorithm(MATRIX_T a [A_ROWS_F][A_COLS_F],
-  //                       MATRIX_T y [B_ROWS_F][B_COLS_F],
-    //                               MATRIX_T x [C_ROWS_F][C_COLS_F]);
 
 
 void sqn_ring_cluster_flatten_system_gen(MATRIX_T cluster_mat1[K][ANTENNA_PER_CLUSTER],
@@ -175,41 +150,6 @@ void sqn_ring_cluster_flatten_system_gen(MATRIX_T cluster_mat1[K][ANTENNA_PER_CL
 					int clust_number);
 
 
-
-template <int W2, int IW2, int W1, int IW1>
-void fxp_sqrt(ap_fixed<W2,IW2>& result, ap_fixed<W1,IW1>& in_val)
-{
-   enum { QW = (IW1+1)/2 + (W2-IW2) + 1 }; // derive max root width
-   enum { SCALE = (W2 - W1) - (IW2 - (IW1+1)/2) }; // scale (shift) to adj initial remainer value
-   enum { ROOT_PREC = QW - (IW1 % 2) };
-   assert((IW1+1)/2 <= IW2); // Check that output format can accommodate full result
-
-   ap_int<QW> q      = 0;   // partial sqrt
-   ap_int<QW> q_star = 0;   // diminished partial sqrt
-   ap_int<QW+2> s; // scaled remainder initialized to extracted input bits
-   if (SCALE >= 0)
-      s = in_val.range(W1-1,0) << (SCALE);
-   else
-      s = ((in_val.range(W1-1,0) >> (0 - (SCALE + 1))) + 1) >> 1;
-
-   // Non-restoring square-root algorithm
-   for (int i = 0; i <= ROOT_PREC; i++) {
-      if (s >= 0) {
-         s = 2 * s - (((ap_int<QW+2>(q) << 2) | 1) << (ROOT_PREC - i));
-         q_star = q << 1;
-         q = (q << 1) | 1;
-      } else {
-         s = 2 * s + (((ap_int<QW+2>(q_star) << 2) | 3) << (ROOT_PREC - i));
-         q = (q_star << 1) | 1;
-         q_star <<= 1;
-      }
-   }
-   // Round result by "extra iteration" method
-   if (s > 0)
-      q = q + 1;
-   // Truncate excess bit and assign to output format
-   result.range(W2-1,0) = ap_int<W2>(q >> 1);
-}
 
 
 template<int R1,int C1, int R2, int C2> //Normal x Normal
@@ -225,33 +165,6 @@ void mat_multiply(MATRIX_T a[R1][C1],MATRIX_T b[R2][C2],MATRIX_T c[R1][C2])
 	}
 }
 
-
-template<int R1,int C1, int R2, int C2> //Conj X Normal
-void mat_multiply1(MATRIX_T a[R1][C1],MATRIX_T b[R2][C2],MATRIX_T c[R1][C2])
-{
-	a_row_loop_m :  for (int row=0;row<R1;row++){
-			b_col_loop:for (int col=0;col<C2;col++){
-				m_loop:for (int m=0;m<C1;m++){
-					#pragma HLS unroll
-					c[row][col]=c[row][col]+ (x_conj(a[m][row])*b[m][col]);
-				}
-			}
-	}
-}
-
-
-template<int R1,int C1, int R2, int C2> //Normal X Conj
-void mat_multiply2(MATRIX_T a[R1][C1],MATRIX_T b[R2][C2],MATRIX_T c[R1][C2])
-{
-	a_row_loop_m :  for (int row=0;row<R1;row++){
-			b_col_loop:for (int col=0;col<C2;col++){
-				m_loop:for (int m=0;m<C1;m++){
-					#pragma HLS unroll
-					c[row][col]=c[row][col]+ (a[row][m]*x_conj(b[col][m]));
-				}
-			}
-	}
-}
 
 
 void f_gradient2(MATRIX_T cluster_mat2[K][K],
@@ -294,8 +207,6 @@ void sqn_star_apex_cluster1(MATRIX_T g2[K][1],
 		MATRIX_T x_init[K][1],
 		int iter);
 
-void sqn_star_alg();
-void sqn_ring_alg();
 
 void sqn_star_frame_new(MATRIX_T y[FRAMES][CLUSTERS][ANTENNA_PER_CLUSTER][1]);
 
